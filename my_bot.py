a60733ba855ae457c04c8d299b2071168dbd8d3c
Gabriel Phelipe <gpcfreitas@gmail.com>
2020-10-23 02:00:08 -0300

    Explicação do código

--- my_bot.py
+++ my_bot.py
@@ -1,9 +1,17 @@
+
+#   O propósito é simples, dar reply em um tweet sempre que a sua conta
+#   for mencionada seguida de uma frase-gatilho (na variável TAG). No caso,
+#   utilizei uma conta fictícia que responde marcações seguidas da frase
+#   "data de hoje" com o respectivo dia da semana, dia do mês, mês e ano.
+
+
 import tweepy
 import re
 from datetime import datetime
 import time
 
-#Todas as 4 variáveis abaixo são geradas e fornecidas após a criação da conta e do App em developer.twitter.com
+
+#Todas as 4 variáveis abaixo são geradas e fornecidas após a criação da conta e do App em https://developer.twitter.com/
 CONSUMER_KEY = 'consumer_key_hash'
 CONSUMER_SECRET = 'consumer_secret_hash'
 ACCESS_KEY = 'access_key_hash'
@@ -15,8 +23,9 @@ api = tweepy.API(auth, wait_on_rate_limit= True)
 
 FILE_NAME = 'last_seen_id.txt'
 
-TAG = "#adatadehoje"
+TAG = "data de hoje"
 
+#Esses dicionários foram criados para a tradução
 months = {
     1: 'janeiro',
     2: 'fevereiro',
@@ -42,30 +51,37 @@ weekdays = {
     'Sunday':'domingo'
 }
 
+#Função de tradução
 def month_in_pt(dictMonth, month_num):
     month_pt = dictMonth[month_num]
     return month_pt
 
+#Função de tradução
 def weekday_in_pt(dictWeekday, weekday_en):
     weekday_pt = dictWeekday[weekday_en]
     return weekday_pt
 
+#Função para ler o arquivo "last_seen_id.txt" e ignorar menções já respondidas
 def read_last_seen(filename):
     f_read = open(filename, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
+#Função para dar upload no arquivo "last_seen_id.txt" com a nova última menção vista
 def store_last_seen(filename, last_seen_id):
     f_write = open(filename, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
     return
 
+#Função principal
 def reply():
-    mentions = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode = 'extended')
-    for mention in reversed(mentions):
-        if "dia de hoje" in mention.full_text.lower():
+    mentions = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode = 'extended') #Acessa o feed das menções feitas
+    for mention in reversed(mentions): #Inverte a ordem da lista de menções para que publicações mais antigas sejam respondidas primeiro
+        if "dia de hoje" in mention.full_text.lower(): #Se existe a frase-gatilho
+            
+            #Linhas 85 à 90 servem para a formatação do Reply
             now = datetime.now()
             day = now.strftime("%d")
             weekday = weekday_in_pt( weekdays ,now.strftime("%A"))
@@ -73,12 +89,17 @@ def reply():
             year = now.strftime("%Y")
             data_de_hoje = ("Hoje é "+ weekday +", "+ day + " de " + month +" de "+ year)
 
-            print(str(mention.id) + ' - ' + mention.full_text)
-            api.update_status("@" + mention.user.screen_name +" "+data_de_hoje, mention.id)
-            api.create_favorite(mention.id)
-            api.retweet(mention.id)
-            store_last_seen(FILE_NAME, mention.id)
+            print(str(mention.id) + ' - ' + mention.full_text) # Para fins de análise e verificação dos ID's
+            api.update_status("@" + mention.user.screen_name +" "+data_de_hoje, mention.id) #cria o reply
+            api.create_favorite(mention.id) #Favorita a menção feita
+            api.retweet(mention.id) #Retweet na menção feita
+            store_last_seen(FILE_NAME, mention.id) #Finalmente, salva o ID da ultima menção respondida, que será também ignorada
 
+#Loop infinito para constante verificação
 while True:
     reply()
     time.sleep(2)
+
+#Em seguida o script foi hospedado em nuvem para que continue o funcionnando =)
+
+#Para mais informações da biblioteca: http://docs.tweepy.org/en/latest/
