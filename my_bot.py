ac75ff5c51e3dfd26870d9c639ee14f76ac31814
Gabriel Phelipe <gpcfreitas@gmail.com>
2023-05-31 16:36:59 -0300

    feat: Updating code

--- my_bot.py
+++ my_bot.py
@@ -1,8 +1,8 @@
 
-#   O propósito é simples, dar reply em um tweet sempre que a sua conta
-#   for mencionada seguida de uma frase-gatilho (na variável TAG). No caso,
-#   utilizei uma conta fictícia que responde marcações seguidas da frase
-#   "data de hoje" com o respectivo dia da semana, dia do mês, mês e ano.
+# The purpose is simple: reply to a tweet whenever your account is mentioned 
+# followed by a trigger phrase (in the TAG variable). In this case, I used 
+# a fictional account that responds to mentions followed by the phrase 
+# "data de hoje" with the corresponding day of the week, day of the month, month, and year.
 
 
 import tweepy
@@ -11,7 +11,7 @@ from datetime import datetime
 import time
 
 
-#Todas as 4 variáveis abaixo são geradas e fornecidas após a criação da conta e do App em https://developer.twitter.com/
+# All 4 variables below are generated and provided after the creation of the account and the App at https://developer.twitter.com/
 CONSUMER_KEY = 'consumer_key_hash'
 CONSUMER_SECRET = 'consumer_secret_hash'
 ACCESS_KEY = 'access_key_hash'
@@ -25,7 +25,7 @@ FILE_NAME = 'last_seen_id.txt'
 
 TAG = "data de hoje"
 
-#Esses dicionários foram criados para a tradução
+#Dictionaries created for translation purposes
 months = {
     1: 'janeiro',
     2: 'fevereiro',
@@ -51,31 +51,31 @@ weekdays = {
     'Sunday':'domingo'
 }
 
-#Função de tradução
+# Month Translation function
 def month_in_pt(dictMonth, month_num):
     month_pt = dictMonth[month_num]
     return month_pt
 
-#Função de tradução
+# Weeks Translation function
 def weekday_in_pt(dictWeekday, weekday_en):
     weekday_pt = dictWeekday[weekday_en]
     return weekday_pt
 
-#Função para ler o arquivo "last_seen_id.txt" e ignorar menções já respondidas
+# Function to read the file "last_seen_id.txt" and ignore mentions that have already been replied to.
 def read_last_seen(filename):
     f_read = open(filename, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
-#Função para dar upload no arquivo "last_seen_id.txt" com a nova última menção vista
+# function to upload the "last_seen_id.txt" file with the last seen mention Id
 def store_last_seen(filename, last_seen_id):
     f_write = open(filename, 'w')
     f_write.write(str(last_seen_id))
     f_write.close()
     return
 
-#Função principal
+# Main Function
 def reply():
     mentions = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode = 'extended') #Acessa o feed das menções feitas
     for mention in reversed(mentions): #Inverte a ordem da lista de menções para que publicações mais antigas sejam respondidas primeiro
@@ -95,11 +95,11 @@ def reply():
             api.retweet(mention.id) #Retweet na menção feita
             store_last_seen(FILE_NAME, mention.id) #Finalmente, salva o ID da ultima menção respondida, que será também ignorada
 
-#Loop infinito para constante verificação
+# Infinite Loop to Make a constant verification
 while True:
     reply()
     time.sleep(2)
 
-#Em seguida o script foi hospedado em nuvem para que continue o funcionnando =)
+# After that the script was hosted to keep working
 
-#Para mais informações da biblioteca: http://docs.tweepy.org/en/latest/
+# For more information about the lib, please check: http://docs.tweepy.org/en/latest/
